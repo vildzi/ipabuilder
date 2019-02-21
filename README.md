d9a3266ff0e9c306789fcd1b188eda99c98eae9d
vilP1L <vilP1l@github.com>
2019-02-21 17:22:17 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# IpaBuilder
+
+Build an ipa from an xcode project. This does not require having a developer account ($100 / yr).  
+This only supports macos at the moment but will gain linux and windows support soon-ish
+
+## Installation
+
+IpaBuilder requires you to have xcode installed (xcodebuild cli).  
+Once you have installed xcode, clone the repo and copy the binary (buildipa) to /usr/local/bin, then you can use `buildipa` from any directory.
+
+## Usage
+
+`buildipa project_dir output_dir`
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import { get } from 'node-cmd';
+import 'colors';
+import { promisify } from 'util';
+import { platform } from 'os';
+import dirExists from 'directory-exists';
+
+const dir = process.argv[2];
+const dirOut = process.argv[3];
+const run = promisify(get);
+
+if (platform() !== 'darwin') {
+  console.log(`${'err'.red} Only macos is currently supported.`);
+  process.exit();
+}
+
+if (!dir || !dirOut) {
+  console.log('Usage: buildipa <project dir> <output dir>');
+  process.exit();
+}
+
+(async () => {
+  if (!await dirExists(dir)) {
+    console.log(`${'err'.red} The specified project directory does not exist.`);
+    process.exit();
+  }
+  if (!await dirExists(dirOut)) {
+    console.log(`${'err'.red} The specified output directory does not exist.`);
+    process.exit();
+  }
+  console.log(`${'info'.blue} Building xcode project...`);
+  if (await run('type -p xcodebuild') === 'xcodebuild not found') {
+    console.log(`${'err'.red} Please install xcode and try again.`);
+    process.exit();
+  }
+  const dirFiles = await run(`cd ${dir} && ls`);
+  if (!dirFiles.includes('.xcodeproj')) {
+    console.log(`${'err'.red} No .xcodeproj was found in the given project directory.`);
+    process.exit();
+  }
+  await run(`cd ${dir} && xcodebuild`);
+  console.log(`${'complete'.green} Finished building xcode project!`);
+  console.log(`${'info'.blue} Building ipa...`);
+  await run(`cd ${dir}/build && mkdir Payload`);
+  const files = await run(`cd ${dir}/build/Release-iphoneos && ls`);
+  const file = files.match(/(.+)\.app\s/g)[0];
+  await run(`cd ${dir}/build && cp -R Release-iphoneos/${file.split('.').shift()}.app Payload`);
+  await run(`cd ${dir}/build && zip -r Payload.zip Payload`);
+  await run(`cd ${dir}/build && mv Payload.zip ${file.split('.').shift()}.ipa`);
+  await run(`cp ${dir}/build/${file.split('.').shift()}.ipa ${dirOut}`);
+  console.log(`${'complete'.green} Finished building ipa!`);
+  console.log(`${'info'.blue} Cleaning up...`);
+  await run(`cd ${dir} && rm -rf build`);
+  console.log(`${'complete'.green} Finished cleaning up!`);
+})();
